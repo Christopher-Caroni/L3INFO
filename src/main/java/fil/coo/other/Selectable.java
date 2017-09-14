97897247d3633fc6bff420f9ca7d8845bdb2a157
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-09-14 20:49:56 +0200

    chooseElement

--- src/main/java/fil/coo/other/Selectable.java
+++ src/main/java/fil/coo/other/Selectable.java
@@ -5,4 +5,5 @@ package fil.coo.other;
  * Any class that could be selected by the user must implement this interface.
  */
 public interface Selectable {
+    int getMenuDescription();
 }
--- src/main/java/fil/coo/util/Menu.java
+++ src/main/java/fil/coo/util/Menu.java
@@ -2,7 +2,9 @@ package fil.coo.util;
 
 import fil.coo.other.Selectable;
 
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
 
@@ -22,8 +24,39 @@ public class Menu {
         return instance;
     }
 
-    public <T extends Selectable> T chooseElement(List<? extends Selectable> possibleMonsters) {
-        // TODO
-        return null;
+    /**
+     * Prints the list of elements and asks the user to choose one with {@link #readChoiceInt(int)}
+     *
+     * @param elementList the list of elements to choose from
+     * @param <T>         must be a selectable type of class
+     * @return the element the user chose
+     */
+    public <T extends Selectable> T chooseElement(List<T> elementList) {
+        System.out.println("Choose an element between 0 and " + (elementList.size() - 1));
+        for (int i = 0; i < elementList.size(); i++) {
+            System.out.println(i + " - " + elementList.get(i).getMenuDescription());
+        }
+
+        int choice = readChoiceInt(elementList.size());
+        return elementList.get(choice);
+    }
+
+    /**
+     * @param upperBound excluded
+     * @return the user's choice
+     */
+    private int readChoiceInt(int upperBound) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+        while (choice <= 0 || choice >= upperBound) {
+            System.out.println("Enter a choice from 0 to " + (upperBound - 1));
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                // consume the input (that is not an integer)
+                scanner.skip(".*");
+            }
+        }
+        return choice;
     }
 }
